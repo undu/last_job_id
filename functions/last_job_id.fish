08088962a63397bf9cbbfcd249c64f42fa04b678
Jorge Bucaran <j@bucaran.me>
2016-04-22 18:57:03 +0900

    Use $argv instead of l to allow user retrieve more than one job.

--- functions/last_job_id.fish
+++ functions/last_job_id.fish
@@ -1,5 +1,5 @@
-function last_job_id -d "Get the id of the last job to be started"
-    jobs -l | awk -v FS=\t '
+function last_job_id -d "Get the id of one or more existing jobs"
+    jobs $argv | awk -v FS=\t '
         /[0-9]+\t/{
             jobs[++job_count] = $1
         }
